db7b8c22612c1580b35525ed2c7acd7990c45167
Mark Yep <ooksei@outlook.com>
2018-04-02 11:49:21 +1200

    Added ISR to test file

--- software/LCFR/freertos_test.c
+++ software/LCFR/freertos_test.c
@@ -2,25 +2,38 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
+#include "system.h"                     // to use the symbolic names
+#include "altera_avalon_pio_regs.h" 	// to use PIO functions
+#include "alt_types.h"                 	// alt_u32 is a kind of alt_types
+#include "sys/alt_irq.h"              	// to register interrupts
 
 /* Scheduler includes. */
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
 
-/* The parameters passed to the reg test tasks.  This is just done to check
- the parameter passing mechanism is working correctly. */
 #define mainREG_TEST_1_PARAMETER    ( ( void * ) 0x12345678 )
 #define mainREG_TEST_2_PARAMETER    ( ( void * ) 0x87654321 )
 #define mainREG_TEST_PRIORITY       ( tskIDLE_PRIORITY + 1)
 static void prvFirstRegTestTask(void *pvParameters);
 static void prvSecondRegTestTask(void *pvParameters);
 
-/*
- * Create the demo tasks then start the scheduler.
- */
+void button_interrupts_function(void* context, alt_u32 id)
+{
+	int* temp = (int*) context;
+	(*temp) = IORD_ALTERA_AVALON_PIO_EDGE_CAP(PUSH_BUTTON_BASE);
+	printf("Hello");
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PUSH_BUTTON_BASE, 0x7);
+}
+
 int main(void)
 {
+	int buttonValue = 0;
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PUSH_BUTTON_BASE, 0x7);
+	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PUSH_BUTTON_BASE, 0x7);
+	alt_irq_register(PUSH_BUTTON_IRQ,(void*)&buttonValue, button_interrupts_function);
+	alt_irq_register(FREQUENCY_ANALYSER_IRQ, (void*)&buttonValue, button_interrupts_function);
+
 	/* The RegTest tasks as described at the top of this file. */
 	xTaskCreate( prvFirstRegTestTask, "Rreg1", configMINIMAL_STACK_SIZE, mainREG_TEST_1_PARAMETER, mainREG_TEST_PRIORITY, NULL);
 	xTaskCreate( prvSecondRegTestTask, "Rreg2", configMINIMAL_STACK_SIZE, mainREG_TEST_2_PARAMETER, mainREG_TEST_PRIORITY, NULL);
@@ -39,8 +52,8 @@ static void prvFirstRegTestTask(void *pvParameters)
 		printf("Task 1\n");
 		vTaskDelay(1000);
 	}
-
 }
+
 static void prvSecondRegTestTask(void *pvParameters)
 {
 	while (1)
