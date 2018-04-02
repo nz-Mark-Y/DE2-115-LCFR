526bf37658aa022f600733f20d7a0cb05421057b
Mark Yep <ooksei@outlook.com>
2018-04-03 10:41:11 +1200

    Modified tasks and added LED control

--- software/LCFR/LCFR_main.c
+++ software/LCFR/LCFR_main.c
@@ -15,31 +15,34 @@
 #include "freertos/queue.h"
 
 /* Definitions. */
-#define mainREG_TEST_1_PARAMETER    ( ( void * ) 0x12345678 )
-#define mainREG_TEST_2_PARAMETER    ( ( void * ) 0x87654321 )
+#define mainREG_DECIDE_PARAMETER    ( ( void * ) 0x12345678 )
+#define mainREG_LED_OUT_PARAMETER   ( ( void * ) 0x87654321 )
+#define mainREG_VGA_OUT_PARAMETER 	( ( void * ) 0x12348765 )
 #define mainREG_TEST_PRIORITY       ( tskIDLE_PRIORITY + 1)
 #define SAMPLE_FREQ 				16000
 
 /* Function Declarations. */
-static void prvFirstRegTestTask(void *pvParameters);
-static void prvSecondRegTestTask(void *pvParameters);
+static void prvDecideTask(void *pvParameters);
+static void prvLEDOutTask(void *pvParameters);
+static void prvVGAOutTask(void *pvParameters);
 
 /* Global Variables. */
 double signalFreq = 0;
 double rocFreq = 0;
+int loads[8] = { 1, 1, 1, 1, 1, 1, 1, 1 };
 
 /* ISRs. */
 void button_interrupts_function(void* context, alt_u32 id)
 {
 	int* temp = (int*) context;
-	(*temp) = IORD_ALTERA_AVALON_PIO_EDGE_CAP(PUSH_BUTTON_BASE);
-	printf("Hello \n");
-	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PUSH_BUTTON_BASE, 0x7);
+	(*temp) = IORD_ALTERA_AVALON_PIO_EDGE_CAP(PUSH_BUTTON_BASE); // Store which button was pressed
+	printf("Hello\n");
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PUSH_BUTTON_BASE, 0x7); // Clear edge capture register
 }
 
 void freq_relay()
 {
-	unsigned int temp = IORD(FREQUENCY_ANALYSER_BASE, 0);
+	unsigned int temp = IORD(FREQUENCY_ANALYSER_BASE, 0); // Get the sample count between the two most recent peaks
 	
 	//Important: do not swap the order of the two operations otherwise the roc will be 0 all the time
 	if (temp > 0)
@@ -54,23 +57,27 @@ void freq_relay()
 /* Main function. */
 int main(void)
 {
+	// Set up Interrupts
 	int buttonValue = 0;
 	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PUSH_BUTTON_BASE, 0x7);
-	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PUSH_BUTTON_BASE, 0x7);
+	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PUSH_BUTTON_BASE, 0x4);
 	alt_irq_register(PUSH_BUTTON_IRQ,(void*)&buttonValue, button_interrupts_function);
 
 	alt_irq_register(FREQUENCY_ANALYSER_IRQ, 0, freq_relay);
 
-	xTaskCreate( prvFirstRegTestTask, "Rreg1", configMINIMAL_STACK_SIZE, mainREG_TEST_1_PARAMETER, mainREG_TEST_PRIORITY, NULL);
-	xTaskCreate( prvSecondRegTestTask, "Rreg2", configMINIMAL_STACK_SIZE, mainREG_TEST_2_PARAMETER, mainREG_TEST_PRIORITY, NULL);
+	// Set up Tasks
+	xTaskCreate( prvDecideTask, "Rreg1", configMINIMAL_STACK_SIZE, mainREG_DECIDE_PARAMETER, mainREG_TEST_PRIORITY, NULL);
+	xTaskCreate( prvLEDOutTask, "Rreg2", configMINIMAL_STACK_SIZE, mainREG_LED_OUT_PARAMETER, mainREG_TEST_PRIORITY, NULL);
+	xTaskCreate( prvVGAOutTask, "Rreg3", configMINIMAL_STACK_SIZE, mainREG_VGA_OUT_PARAMETER, mainREG_TEST_PRIORITY, NULL);
 
 	vTaskStartScheduler();
 
+	// Only reaches here if not enough heap space to start tasks
 	for(;;);
 }
 
 /* Tasks. */
-static void prvFirstRegTestTask(void *pvParameters)
+static void prvDecideTask(void *pvParameters)
 {
 	while (1)
 	{
@@ -79,7 +86,24 @@ static void prvFirstRegTestTask(void *pvParameters)
 	}
 }
 
-static void prvSecondRegTestTask(void *pvParameters)
+static void prvLEDOutTask(void *pvParameters)
+{
+	while (1)
+	{
+		int loads_num = 0;
+		int i;
+		for (i = 0; i < 8; i++)
+		{
+			loads_num = loads_num << 1;
+			loads_num = loads_num + loads[i];
+		}
+		printf("%d \n", loads_num);
+		IOWR_ALTERA_AVALON_PIO_DATA(GREEN_LEDS_BASE, loads_num);
+		vTaskDelay(100);
+	}
+}
+
+static void prvVGAOutTask(void *pvParameters)
 {
 	while (1)
 	{
